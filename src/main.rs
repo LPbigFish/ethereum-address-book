9627a629f68c08603aca6b2e15ab0046e38617fa
LPbigFish <lpbyblock@gmail.com>
2024-12-11 21:56:00 +0100

    Added the whole filtering, now what I need to do is to automate it.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,59 @@
 use std::collections::HashSet;
 use std::env;
+use std::iter::once;
+use std::sync::{Arc, Mutex};
+use alloy::consensus::Transaction;
 use alloy::eips::BlockNumberOrTag;
 use alloy::network::primitives::BlockTransactionsKind;
+use alloy::primitives::Address;
 use alloy::providers::{Provider, ProviderBuilder};
-use tokio::sync::RwLock;
 use reqwest::Url;
 
 struct AddressBook {
     block_height: u64,
-    addresses: RwLock<HashSet<[u8; 20]>>
+    addresses: HashSet<[u8; 20]>
+}
+
+impl AddressBook {
+    fn new(block_height: u64, addresses: HashSet<[u8; 20]>) -> AddressBook {
+        AddressBook { block_height, addresses }
+    }
+
+    fn increment_block_height(&mut self) -> u64 {
+        self.block_height += 1;
+        self.block_height.clone()
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv()?;
 
-    let rpc_url = env::var("ALCHEMY_URL")?;
-    let provider = ProviderBuilder::new().on_http(Url::parse(rpc_url.as_str()).unwrap());
+    let address_book = Arc::new(Mutex::new(AddressBook::new(21381144, HashSet::new())));
 
-    if let Some(block) = provider.get_block_by_number(BlockNumberOrTag::Number(21374922), BlockTransactionsKind::Full)
-        .await? {
-        println!("Block Hash: {:?}", block.transactions.as_transactions().unwrap()[0].block_number.unwrap());
+    let rpc_url = Arc::new(env::var("ALCHEMY_URL").expect("Alchemy URL not set").to_string());
+
+    let provider = ProviderBuilder::new().on_http(Url::parse(rpc_url.as_str()).expect("Http URL not set"));
+
+    let binding = Arc::clone(&address_book);
+    let mut value = binding.lock().unwrap();
+
+    if let Ok(Some(block)) = provider.get_block_by_number(BlockNumberOrTag::Number((*value).increment_block_height()), BlockTransactionsKind::Full).await {
+        let block_addresses: HashSet<[u8;20]> = block.transactions.into_transactions().map(|transaction| {
+            if let Some(addressed) = transaction.to() {
+                addressed.into_array()
+            } else {
+                transaction.from.into_array()
+            }
+        }).collect();
+        once(block.header.beneficiary.into_array()).chain(block_addresses).for_each(| address | {
+            (*value).addresses.insert(address);
+        });
+    } else {
+        println!("No block found in provider");
     }
+    let stringify_addresses = (*value).addresses.iter().map(|add| Address::from(add).to_string()).collect::<Vec<String>>();
+    println!("{:?}", stringify_addresses);
 
     Ok(())
 }
